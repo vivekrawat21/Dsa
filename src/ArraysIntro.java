5bf1d9b173ec3238d584e8f1a9d101e8bc459ca9
vivekrawat21 <vivekrwt2111@gmail.com>
2023-07-12 20:46:20 +0530

    2d array started

--- src/ArraysIntro.java
+++ src/ArraysIntro.java
@@ -1,5 +1,13 @@
 public class ArraysIntro {
     public static void main(String[] args) {
 
+         int[] Numbers= new int[4];
+         Numbers[0]=33;
+         Numbers[1]=4;
+        Numbers[2]=5;
+        System.out.println(Numbers[1]+Numbers[2]);
+
+
+
     }
 }

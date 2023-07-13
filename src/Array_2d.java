0055d1cb0cf46e0df38bc0cd35b8eb26548f68c2
vivekrawat21 <vivekrwt2111@gmail.com>
2023-07-14 01:05:28 +0530

    on 2d array

---
+++ src/Array_2d.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Array_2d {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+//        int[] Array1d= new int[3];
+//        for (int element: Array1d){
+//            Array1d[element]= in.nextInt();
+//        }
+//
+//        System.out.println(Arrays.toString(Array1d));     ==> 1-Dimensionla array with advance for loop which is known as foreach loop  This is only for exaple purposes
+
+
+        int[][] arr = new int[2][2];
+
+        System.out.println("Enter your array");
+        for (int row =0;row<arr.length;row++){
+            for (int col=0 ; col< arr[row].length ; col++){
+                arr[row][col] = in.nextInt();
+            }
+        }
+        for (int row =0;row<arr.length;row++){
+            for (int col=0 ; col< arr[row].length ; col++){
+                System.out.print(arr[row][col]+" ");
+            }
+            System.out.println();
+        }
+        for (int[] num:arr) {
+                System.out.println(Arrays.toString(num));
+
+        }
+
+    }
+}

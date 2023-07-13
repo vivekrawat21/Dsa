e8b96e4478decb516ff5a792412c1831f671b220
vivekrawat21 <vivekrwt2111@gmail.com>
2023-07-14 01:51:00 +0530

    Advance arrayList

--- src/ARRAYLIST.java
+++ src/ARRAYLIST.java
@@ -9,16 +9,28 @@ public class ARRAYLIST {
 
         Scanner in = new Scanner(System.in);
 
-        dynamicArray.add(67);
-        dynamicArray.add(672);
-        dynamicArray.add(6732);
-        dynamicArray.add(6723);
-        dynamicArray.add(67232);
-
-        System.out.println(dynamicArray);//Internally calling .toString in this
+//        dynamicArray.add(67);
+//        dynamicArray.add(672);
+//        dynamicArray.add(6732);
+//        dynamicArray.add(6723);
+//        dynamicArray.add(67232);
+//
+//        System.out.println(dynamicArray);//Internally calling .toString in this
         // there are many methods like
 //        dynamicArray.add(),dynamicArray.contains(),dynamicArray.remove(),dynamicArray.equals()
 
+        System.out.print("Enter how many no. you want to store: ");
+        int num =in.nextInt();
+        System.out.println("Enter "+num+ (num>=10?" numbers":" number"));
+        try{
+        for (int i = 0; i < num; i++) {
+            dynamicArray.add(in.nextInt());
 
+        }
+            System.out.print("Your Array is: ");
+        System.out.println(dynamicArray);}
+        catch (Exception e){
+            System.out.println("please Enter valid datatype: "+e.getMessage());
+        }
     }
 }

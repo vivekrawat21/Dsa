bf30cc7f29d0f430b1e6a2d12393a27570073efc
vivekrawat21 <vivekrwt2111@gmail.com>
2023-07-14 22:21:08 +0530

    Range Search

--- src/LinearSearchString.java
+++ src/LinearSearchString.java
@@ -1,12 +1,24 @@
 import java.util.Scanner;
 
 public class LinearSearchString {
-    static Boolean LinearSearch(String s,char target) {
+//    static Boolean LinearSearch(String s,char target) {
+//        if (s.length() == 0) {
+//            return false;
+//        }
+//        for (int i = 0; i < s.length(); i++) {
+//            if (target==s.charAt(i)){
+//                return true;
+//
+//            }
+//        }
+//        return false;
+//    }
+    static Boolean LinearSearch2(String s,char target) {
         if (s.length() == 0) {
             return false;
         }
-        else for (int i = 0; i < s.length(); i++) {
-            if (target==s.charAt(i)){
+        for (char ch : s.toCharArray()){
+            if (target==ch){
                 return true;
 
             }
@@ -14,11 +26,12 @@ public class LinearSearchString {
         return false;
     }
 
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.print("Enter the name: ");
         String name = in.next();
          char target = 's';
-        System.out.println(LinearSearch(name,target)==true?("character is found"):("Character not found"));
+        System.out.println(LinearSearch2(name,target)==true?("character is found"):("Character not found"));
     }
 }
---
+++ src/SearchInRange.java
@@ -0,0 +1,31 @@
+public class SearchInRange {
+    public static void main(String[] args) {
+
+        int[] arr = {1, 2, 3, 2, 23, 34, 4, 5, 3, 3};
+        int target = 5;
+        int start =1;
+        int end =5;
+
+        System.out.println(linearSearch(arr,target,start,end)==-1?("Element Not found"):("Element found at index: "+linearSearch(arr,target,start,end)));
+
+
+
+    }
+
+    static int linearSearch(int[] arr, int target,int Start_index, int End_index) {
+        if (arr.length == 0) {
+            return -1;
+        }
+        for (int index = Start_index; index <= End_index; index++) {
+            int element = arr[index];
+            if (element == target) {
+                return index;
+
+            }
+        }
+        //this will execute if none of the return statements will be returned
+        return -1;
+
+    }
+
+}

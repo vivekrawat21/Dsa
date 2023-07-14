cc07781fe2f3ba9f8be5e7a6afe3e9a6bc788b6a
vivekrawat21 <vivekrwt2111@gmail.com>
2023-07-14 23:08:10 +0530

    Binary Search

--- src/MinimumNumberInArray.java
+++ src/MinimumNumberInArray.java
@@ -1,2 +1,27 @@
+import java.util.Scanner;
+
 public class MinimumNumberInArray {
+    public static void main(String[] args) {
+        //Searching minimum element in array
+        int[] arr= new int[12];
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter element in your array: ");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i]= in.nextInt();
+        }
+        System.out.println("MinNumber in your array is "+ SearchMin(arr));
+    }
+    static int SearchMin(int[] ar ){
+        int minNum=ar[0];
+        if (ar.length==0){
+            return -1;
+        }
+        for (int i=0; i<ar.length;i++){
+
+         if (ar[i]<minNum) {
+             minNum=ar[i];
+         }
+        }
+        return minNum;
+    }
 }

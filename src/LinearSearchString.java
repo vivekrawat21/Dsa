c35ff02dc472c4ee3c5a1239aa196a1d40ac7f5c
vivekrawat21 <vivekrwt2111@gmail.com>
2023-07-17 22:52:17 +0530

    today question

--- src/LinearSearchString.java
+++ src/LinearSearchString.java
@@ -32,6 +32,6 @@ public class LinearSearchString {
         System.out.print("Enter the name: ");
         String name = in.next();
          char target = 's';
-        System.out.println(LinearSearch2(name,target)==true?("character is found"):("Character not found"));
+        System.out.println(LinearSearch2(name,target)==true?("character "+target+" is found"):("Character not found"));
     }
 }

a224de7d63d9c9185987f0ff63981111711ff061
vivekrawat21 <vivekrwt2111@gmail.com>
2023-07-19 23:01:19 +0530

    mthod for cealing from kunal

--- src/binarySearchQuestions.java
+++ src/binarySearchQuestions.java
@@ -7,43 +7,34 @@ public class binarySearchQuestions {
         //ex : target=14 the cealing =14 target= 15 ceiling= 16
 // For Binary search first we should know that the array will always be sorted- you see sorted array apply binary search
          int[] arr= {-3,-1,0,2,7,10,19,44,45,55};
-        System.out.println(cealingCealing(arr,-6));
+        System.out.println(cealingSearch(arr,-2));
+
 
     }
 
-    static int cealingCealing(int[] arr , int target){
-        int[] sealing = new int[30];
-        int j=0;
-
-        for (int i = 0; i <arr.length ; i++) {
-            {
-                if(arr[i]>= target){
-
-                    sealing[j]=arr[i];
+    //This is only different from binary search only when the array only do not contain the target
+    static int cealingSearch(int[] arr, int target) {
+        int  end= arr.length-1;
+        int start = 0;
 
 
+        while(start<=end){  //If the target is not find then in the last condition the start will be greater then the end so in that case the start will be end+1 and there is where we find our answer if the target element is not found then the element greater than that in the array will be the answer..
+            int mid = start+ (end-start)/2;
+            if(arr[mid]==target){
 
-
-                }
-                j++;
-
-
+                return arr[mid];
             }
-
-
-        }
-        int min=Integer.MAX_VALUE;
-        for (int i = 0; i < sealing.length; i++) {
-
-            if (sealing[i]<min ){
-                min= sealing[i];
+            else if(target<arr[mid]){
+                end=mid-1;
             }
-
+            else {
+                start=mid+1;
+            }
         }
 
-
-        return min;
+        return arr[start];
     }
+
 
 }
 

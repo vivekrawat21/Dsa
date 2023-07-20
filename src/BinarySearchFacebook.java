f5ef05f7517268c7ad10f96749275627a009a3d7
vivekrawat21 <vivekrwt2111@gmail.com>
2023-07-20 23:32:59 +0530

    facebook question

--- src/BinarySearchFacebook.java
+++ src/BinarySearchFacebook.java
@@ -1,6 +1,6 @@
 public class BinarySearchFacebook {
     //Find the first and last index of target number;
-    //
+    //For finding that we have to take the approach of binary search as follows
     public static void main(String[] args) {
 
     }

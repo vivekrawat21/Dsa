1444d1ce25a71506b7c7a688b1b89243b869228a
vivekrawat21 <vivekrwt2111@gmail.com>
2023-07-23 20:40:47 +0530

    Peak index in mountain array problem

--- src/moutainArray.java
+++ src/moutainArray.java
@@ -2,6 +2,7 @@ public class moutainArray { //also known as bitonic/ mountain array
 
     //First it's increasing then decreasing.
     //we have to find the peak of  or highest number in this array
+    //for finding that first we have to apply binary search on both sides
 
 
 }

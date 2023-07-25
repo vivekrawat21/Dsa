bcc8dfff7b5e1c85b0e0faccee87d1cb9a5f043f
vivekrawat21 <vivekrwt2111@gmail.com>
2023-07-25 22:53:58 +0530

    resolving this question

--- src/SearchInRotatedSortedArray.java
+++ src/SearchInRotatedSortedArray.java
@@ -31,27 +31,25 @@ public class SearchInRotatedSortedArray {
         }
 
     }
-    static int findPivot(int[] arr){
+    static int findPivot(int[] arr) {
         int start=0;
         int end = arr.length-1;
-        while(start<=end){
-            //4 cases here
-            int mid= start+(end-start)/2;
-            if(mid<end && arr[mid]>arr[mid+1]){ //In this mid should be less than end otherwise out of bound error
+        while(start<=end) {
+            int mid = start+(end-start)/2;
+            if(mid<end && arr[mid]>arr[mid+1]) {
                 return mid;
-
             } else if ( mid>start && arr[mid]<arr[mid-1]) {
                 return mid-1;
-
-            } else if (arr[mid]<=arr[start]) {
-                end= mid-1;
+            } else if (arr[start] >= arr[mid]) {
+                end = mid-1;
             } else {
-                start= mid+1;
+                start = mid+1;
             }
-
         }
         return -1;
     }
+
+
     static int binarySearch(int[] sortedArr ,int target,int min, int max){
 
         while(min<=max) {

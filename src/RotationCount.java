44a747e4e55d6dd58450e7f6da600c870bbf2e6d
vivekrawat21 <vivekrwt2111@gmail.com>
2023-07-26 23:50:24 +0530

    search in RotatedArray when the array contains duplicate elements

--- src/RotationCount.java
+++ src/RotationCount.java
@@ -1,7 +1,8 @@
 public class RotationCount {
 
     public static void main(String[] args) {
-        int []arr={4,5,6,7,0,1,2};
+        int []arr={1,2,3,4,5,6,7,8};
+        //This will return 0 cause this is not rotated. cause(-1+1 = 0)
         System.out.println(countRotations(arr));
     }
     private static int countRotations(int[] arr){

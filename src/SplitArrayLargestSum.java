11ce9849471de0fbc4379cc738f18720827e6185
vivekrawat21 <vivekrwt2111@gmail.com>
2023-07-27 23:14:51 +0530

    SplitArrayLargestSum-Hard question

--- src/SplitArrayLargestSum.java
+++ src/SplitArrayLargestSum.java
@@ -1,4 +1,6 @@
 public class SplitArrayLargestSum {
+    //In this question we have to find the minimum sums from the largest sum that is possible from the partitions
+    //For example arr={2,3,4,5,6,7,9,0} partition1={2,3,4} partition2={5,6,7,9,0} so the max partition sum  is partition 2 which is 5+6+7+9+0 which is 27 so the we have to make partiition like that and find the parttion which is maximum but have to minimum when we ccomparte it with all the partitions
     public static void main(String[] args) {
 
     }

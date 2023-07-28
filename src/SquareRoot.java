d53b9b357fb2b7373c7d63a64f516cff629d8d22
vivekrawat21 <vivekrwt2111@gmail.com>
2023-07-28 22:52:14 +0530

    squareRoot using binary search

--- src/SquareRoot.java
+++ src/SquareRoot.java
@@ -1,5 +1,9 @@
 public class SquareRoot {
     public static void main(String[] args) {
         //Here i will try to find a squareroot using binary search
+        //The squareroot shoule be of integer no float
+        //This can be using  the method of finding floor of a number
+
+
     }
 }

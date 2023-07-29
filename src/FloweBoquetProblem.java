cda95886dfbd9b760cf7feff1e8169a2bf41e546
vivekrawat21 <vivekrwt2111@gmail.com>
2023-07-29 23:15:08 +0530

    logic 1

--- src/FloweBoquetProblem.java
+++ src/FloweBoquetProblem.java
@@ -2,4 +2,23 @@ public class FloweBoquetProblem {
     public static void main(String[] args) {
 //flower bouquet problem
     }
+    static Boolean possible(int[] arr, int day, int m ,int k){
+        int count=0;
+        int noOfBoquet=0;
+        for (int i = 0; i < arr.length-1 ; i++) {
+            if (arr[i]>=day){
+                count++;
+            }
+            else{
+               noOfBoquet+= count/k;
+               count=0;
+            }
+
+        }
+        noOfBoquet+=count/k;
+        if(noOfBoquet>=m) {
+            return true;
+        }
+        return false;
+    }
 }

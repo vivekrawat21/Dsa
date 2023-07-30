b1621d33a3b0dcb0855315ace8a7b0fcf70a08fa
vivekrawat21 <vivekrwt2111@gmail.com>
2023-07-30 23:44:33 +0530

    Sorting started

--- src/Sorting/BubbleSort.java
+++ src/Sorting/BubbleSort.java
@@ -4,6 +4,7 @@ public class BubbleSort {
     public static void main(String[] args) {
         //Bubble sort also known as sinking sort and exchange sort
         //Sorting may be ascending or descending
+        //complexity of bubble sort is O(n^2)
 
     }
 }

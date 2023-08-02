410fb85062e5d0040e2e0e993896ebc6f64d1170
vivekrawat21 <vivekrwt2111@gmail.com>
2023-08-02 23:33:42 +0530

    Revision2

--- src/Revision/BubbleSort.java
+++ src/Revision/BubbleSort.java
@@ -6,19 +6,27 @@ public class BubbleSort {
 
 
     public static void main(String[] args) {
-        int []arr={1,3,4,5,6,123,2,14,6,7,43,5};
+        int []arr={1,2,3,4,8,7,7,8,9};
         Sort(arr);
         System.out.println(Arrays.toString(arr));
     }
+//    making it more efficient
     static void Sort(int[] arr){
      for (int i=0;i<arr.length;i++){
+         boolean swapped= false;
          for(int j=0; j<arr.length-i-1;j++){
              if(arr[j]>arr[j+1]){
                  int temp = arr[j];
                  arr[j]=arr[j+1];
                  arr[j+1]=temp;
+                 swapped =true;
              }
+
+             }
+         if(swapped==false){
+             break;
          }
+
      }
 
     }

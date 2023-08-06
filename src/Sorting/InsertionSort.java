78320bf8eee29d74571299812289a64b7379d796
vivekrawat21 <vivekrwt2111@gmail.com>
2023-08-06 23:54:41 +0530

    InsertionSort Done

--- src/Sorting/InsertionSort.java
+++ src/Sorting/InsertionSort.java
@@ -1,7 +1,33 @@
 package Sorting;
 
+import java.util.Arrays;
+
 public class InsertionSort {
+
+    //After every pass array is getting sorted....
     public static void main(String[] args) {
+
+        int[] arr = {2,1,3,5,8,3,23,23,33,22,21};
+        sortInsertion(arr);
+        System.out.println(Arrays.toString(arr));
+
+
+
+    }
+    static void sortInsertion(int[]arr){
+        for (int i = 0; i < arr.length-1; i++) {
+            for (int j =i+1; j > 0; j--) {
+                if (arr[j-1]>arr[j]){
+                    int temp= arr[j];
+                    arr[j]=arr[j-1];
+                    arr[j-1]= temp;
+                }
+                else if(arr[j-1]<arr[j]) {
+                    break;
+                }
+            }
+
+        }
 
     }
 }

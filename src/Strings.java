ea020b8af705fc8cb03b0d82453cd68db0519608
vivekrawat21 <vivekrwt2111@gmail.com>
2023-08-08 23:42:19 +0530

    Strings

---
+++ src/Strings.java
@@ -0,0 +1,9 @@
+public class Strings {
+    public static void main(String[] args) {
+        String name= "Vivek Rawat";
+        String B ="Vivek Rawat";
+        //Both the refrence variable pointing to same memory space in heap
+        System.out.println(B);
+        //Strings in java are immutable
+    }
+}

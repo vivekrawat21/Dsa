ea6bea704f37da0029e5e3cf11a79ec6d08e5412
vivekrawat21 <vivekrwt2111@gmail.com>
2023-08-09 22:34:33 +0530

    Preety string

---
+++ src/PreetyPrinting.java
@@ -0,0 +1,8 @@
+public class PreetyPrinting {
+    public static void main(String[] args) {
+        //want to print two digits
+        float a= 1212.121212f;
+        System.out.printf("Formatted number is %.2f",a);
+
+    }
+}
--- src/Strings.java
+++ src/Strings.java
@@ -2,6 +2,8 @@ public class Strings {
     public static void main(String[] args) {
         String name= "Vivek Rawat";
         String B ="Vivek Rawat";
+        Integer num = 33;
+        System.out.println(num);
         //Both the refrence variable pointing to same memory space in heap
 //        System.out.println(B);
         //Strings in java are immutable

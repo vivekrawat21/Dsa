d5ec13e0c797a2d226592d42d7cf642ed0f77374
vivekrawat21 <vivekrwt2111@gmail.com>
2023-08-10 22:50:35 +0530

    Refactored

--- src/Strings/Performance.java
+++ src/Strings/Performance.java
@@ -2,6 +2,7 @@ package Strings;
 public class Performance {
     public static void main(String[] args) {
      String series="";
+
        for (int i=0 ; i<26;i++){ //This is not good practice in this first the string copy the before string then append the i and then print it .In the End it increase storage. It lead to wastage of storage...
            char ch = (char) ('a' + i);
            series= series+ch; //We can add character like this ..

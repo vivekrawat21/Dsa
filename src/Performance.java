6d16532050de799f9a836847bea5b2a894821914
vivekrawat21 <vivekrwt2111@gmail.com>
2023-08-10 22:19:27 +0530

    Performance in strings

---
+++ src/Performance.java
@@ -0,0 +1,11 @@
+public class Performance {
+    public static void main(String[] args) {
+       String series="";
+       for (int i=0 ; i<26;i++){
+           char ch = (char) ('a' + i);
+           series= series+ch; //We can add character like this ..
+
+        }
+        System.out.println(series);
+    }
+}
--- src/PreetyPrinting.java
+++ src/PreetyPrinting.java
@@ -1,8 +1,14 @@
+import java.util.ArrayList;
+
 public class PreetyPrinting {
     public static void main(String[] args) {
         //want to print two digits
         float a= 1212.121212f;
-        System.out.printf("Formatted number is %.2f",a);
-
+        System.out.printf("Formatted number is %.2f \n",a);
+        ArrayList<Integer> array= new ArrayList<Integer>();
+        array.add(5);
+        array.add(9);
+        System.out.println(array);
+        //Object and every datatypes which are object will call toString before calling sout
     }
 }

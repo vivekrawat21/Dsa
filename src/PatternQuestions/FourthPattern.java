15398748c9a9307de9e779aea4efacf6e5aa7b3d
vivekrawat21 <vivekrwt2111@gmail.com>
2023-08-13 23:25:51 +0530

    PatternThree

---
+++ src/PatternQuestions/FourthPattern.java
@@ -0,0 +1,27 @@
+package PatternQuestions;
+
+public class FourthPattern {
+
+    public static void main(String[] args) {
+        //Approach:
+        //no. of lines == no.of rows == no. of time the outer loop will run.
+
+        //Identify for every row number how many columns are there or types of elements in the column
+
+        //What do you need to print for every element
+        patternFirst(6);
+    }
+
+    static void patternFirst(int n){
+        for (int row = 1; row <= n; row++) {
+            //for every row how many colums are = row
+            int count =0;
+            for (int col=1; col<=row;col++){
+
+                count++;
+                System.out.print(count + " " );
+            }
+            System.out.println();
+        }
+    }
+}

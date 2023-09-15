0d6c0d02ae8897eec0ecd6c63a94aaba8b454ce4
vivekrawat21 <vivekrwt2111@gmail.com>
2023-09-15 12:31:41 +0530

    set the ith bit

--- src/BitwiseOperators/SetIthBit.java
+++ src/BitwiseOperators/SetIthBit.java
@@ -2,15 +2,15 @@ package BitwiseOperators;
 
 import java.util.Scanner;
 
-public class SetIthBit {
+public class SetIthBit{
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the number you want to set the nth bit: ");
-        int num = sc.nextInt();
-        int shift = num>>(num-1);
-        int setBit = shift | 1;
-        System.out.println(setBit);
+       int num = 9;
+       int i = 3;
+        System.out.println(SetBit(num,i));
 
+    }
 
+    private static int SetBit(int num , int i){
+        return num | (1<<(i-1));
     }
 }

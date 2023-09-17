0f0b13d0871db42e43998cc23fd1d1389e0e81b0
vivekrawat21 <vivekrwt2111@gmail.com>
2023-09-17 22:00:41 +0530

    Xor in range

--- src/BitwiseOperators/XorOfNumbersAndRangeXor.java
+++ src/BitwiseOperators/XorOfNumbersAndRangeXor.java
@@ -5,10 +5,16 @@ import java.util.Scanner;
 public class XorOfNumbersAndRangeXor {
 
     public static void main(String[] args) {
-        System.out.print("Enter till number you want to XOR: ");
         Scanner in = new Scanner(System.in);
-        int num = in.nextInt();
-        System.out.println("Xor of till the number : "+ Xor(num));
+//        System.out.print("Enter till number you want to XOR: ");
+        System.out.print("Enter the range of the number you want to store: "+"\nFrom: ");
+        int a = in.nextInt();
+        System.out.print("till: ");
+        int b = in.nextInt();
+        System.out.println(Xor(b)^Xor(a-1));
+
+//        int num = in.nextInt();
+//        System.out.println("Xor of till the number : "+ Xor(num));
     }
 
     private static int Xor(int num) {

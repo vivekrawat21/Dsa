3438d6df0f615e9ef224952248ec2c1c24997f06
vivekrawat21 <vivekrwt2111@gmail.com>
2023-10-03 09:15:27 +0530

    CountingNumberOfzeros

---
+++ src/Recursion/level1/CountingNumberOfzeros.java
@@ -0,0 +1,29 @@
+package Recursion.level1;
+
+import java.util.Scanner;
+
+public class CountingNumberOfzeros
+{
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        int number = in.nextInt();
+        System.out.println( CountingZeros( number));
+
+    }
+    private static int CountingZeros(int num){
+        return helper(num,0);
+    }
+    private static int helper(int num , int count){
+        if (num==0){
+            return count;
+        }
+       int  rem = num%10;
+        if(rem ==0){
+            return helper(num/10,count+1);
+        }
+        else {
+            return helper(num/10,count);
+        }
+    }
+}

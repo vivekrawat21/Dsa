74b2ebafe96e8a04265ef0d1088d15667d1fa4c4
vivekrawat21 <vivekrwt2111@gmail.com>
2023-10-14 13:47:06 +0530

    LinearSearchFindingIndex

---
+++ src/Recursion/RecursionArray/LinearSearchRecursion.java
@@ -0,0 +1,58 @@
+package Recursion.RecursionArray;
+
+import java.util.Scanner;
+
+public class LinearSearchRecursion{
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int [] arr= {2,3,45,3,21,21,33,234};
+        System.out.print("Enter the target you want to search: ");
+       int target = in.nextInt();
+        String finded = Search(arr, target,0) ? target+" is present at index from first is: " : "The target is not present in the array!";
+        System.out.println(finded+FindIndex(arr , target , 0));
+
+        String findedd = Search(arr, target, 0) ? target + " is present at index from Last is: " : "The target is not present in the array!";
+        System.out.println(findedd+FindFromLastIndex(arr , target , arr.length-1));
+    }
+
+    private static int FindIndex(int[] arr, int target, int index) {
+        if (index==arr.length)
+        {
+            return -1;
+        }
+        else if (arr[index]==target) {
+
+            return index;
+
+        }
+        else {
+            return FindIndex(arr, target,++index);
+        }
+
+    }
+
+    private static boolean Search(int[] arr, int target, int index) {
+        if(index==arr.length){
+            return false;
+        }
+        else {
+            return arr[index]==target || Search(arr,target,index+1);
+        }
+
+    }
+    private static int FindFromLastIndex(int[] arr, int target, int index) {
+        if (index<0)
+        {
+            return -1;
+        }
+        else if (arr[index]==target) {
+
+            return index;
+
+        }
+        else {
+            return FindFromLastIndex(arr, target,index-1);
+        }
+
+    }
+}
